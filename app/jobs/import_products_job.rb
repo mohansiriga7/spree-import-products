be0df9af6dcb9293a15aea80f9edc7da6831c027
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-07-22 12:04:38 +0300

    Fix mail_from delyed_job crash

--- app/jobs/import_products_job.rb
+++ app/jobs/import_products_job.rb
@@ -1,30 +1,34 @@
 class ImportProductsJob < ActiveJob::Base
   queue_as :default
 
-	after_perform :notify_admin
+  after_perform :notify_admin
 
   rescue_from(StandardError) do |exception|
     Rails.logger.error("[ActiveJob] [#{self.class}] [#{job_id}] ID: #{@product_id} #{exception}")
-		products=Spree::ProductImport.find(@product_id)
-		user=Spree::User.find(products.created_by)
-    Spree::UserMailer.product_import_results(user, exception.message+" "+exception.backtrace.join("\n")).deliver
+    products = Spree::ProductImport.find(@product_id)
+    store = Spree::Store.find(@store_id)
+    user = Spree::User.find(products.created_by)
+    Spree::UserMailer.product_import_results(user, store, exception.message + " " + exception.backtrace.join("\n")).deliver
     #Hacemos aquí el update para que aunque falle, enviemos el mail igualmente
-    products.error_message=exception.message
+    products.error_message = exception.message
     products.failure
   end
 
-  def perform(product_id)
-		@product_id=product_id
-    products=Spree::ProductImport.find(product_id)
+  def perform(product_id, current_store_id)
+    @product_id = product_id
+    @store_id = current_store_id
+    products = Spree::ProductImport.find(product_id)
     products.import_data!(true)
   end
 
   def notify_admin
-		products=Spree::ProductImport.find(@product_id)
-		user=Spree::User.find(products.created_by)
-		#log("USER: #{user.email}")
-    Spree::UserMailer.product_import_results(user).deliver
+    products = Spree::ProductImport.find(@product_id)
+    store = Spree::ProductImport.find(@store_id)
+    user = Spree::User.find(products.created_by)
+    #log("USER: #{user.email}")
+    Spree::UserMailer.product_import_results(user, store).deliver
   end
+
   private
   def log(message, severity = :info)
     @rake_log ||= ActiveSupport::Logger.new('log/import_products.log')
--- lib/spree_import_products/user_mailer_ext.rb
+++ lib/spree_import_products/user_mailer_ext.rb
@@ -2,10 +2,10 @@ module SpreeImportProducts
   module UserMailerExt
     def self.included(base)
       base.class_eval do
-        def product_import_results(user, error_message = nil)
+        def product_import_results(user, store, error_message = nil)
           @user = user
           @error_message = error_message
-          from=Spree::Config.mails_from
+          from = store.mail_from_address
           #attachments["import_products.log"] = File.read(Spree::ProductImport.settings[:log_to]) if @error_message.nil?
           mail(:to => @user.email, :from => from, :subject => "Spree: Import Products #{error_message.nil? ? "Success" : "Failure"}")
         end
