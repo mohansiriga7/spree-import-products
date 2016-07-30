cc2765a79d5f0a6ac415d1fd65174e9ac4a6c60d
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2016-07-30 13:40:27 +0300

    Fix url and file path for uploads

--- app/models/spree/product_import.rb
+++ app/models/spree/product_import.rb
@@ -12,7 +12,10 @@ module Spree
 
     ENCODINGS= %w(UTF-8 iso-8859-1)
 
-    has_attached_file :data_file, :path => "public/product_data/data-files/:basename_:timestamp.:extension"
+    has_attached_file :data_file,
+      :path => "public/spree/product_imports/data-files/:basename_:timestamp.:extension",
+      :url => "/spree/product_imports/data-files/:basename_:timestamp.:extension"
+
     validates_attachment_presence :data_file
     #Content type of csv vary in different browsers.
     validates_attachment :data_file, :presence => true, content_type: { content_type: ["text/csv", "text/plain", "text/comma-separated-values", "application/octet-stream", "application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"] }
