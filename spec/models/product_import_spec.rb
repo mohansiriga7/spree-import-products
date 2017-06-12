e8aa0da0659e419fe232e92d0a24eeb2af52b53a
Cooper LeBrun <cooperlebrun@gmail.com>
2017-06-12 14:19:57 -0700

    Major syntax/best practices clean up

--- spec/models/product_import_spec.rb
+++ spec/models/product_import_spec.rb
@@ -7,60 +7,61 @@ module Spree
         product; size; color; option_color; option_size
       end
 
-      let(:size) { FactoryGirl.create(:option_type, :name => "tshirt-size") }
-      let(:color) { FactoryGirl.create(:option_type, :name => "tshirt-color", :presentation => "Color") }
-      let(:option_color) { FactoryGirl.create(:option_value, :name => "blue", :presentation => "Blue", :option_type => color) }
       # let(:product) { FactoryGirl.create(:product, :sku => "001", :permalink => "S0388G-bloch-kids-tap-flexewe") } # UndefinedMethod: permalink=
       let(:product) { FactoryGirl.create(:product, sku: '001') }
+      let(:size) { FactoryGirl.create(:option_type, name: 'tshirt-size') }
+      let(:color) { FactoryGirl.create(:option_type, name: 'tshirt-color', presentation: 'Color') }
+      let(:option_color) { FactoryGirl.create(:option_value, name: 'blue', presentation: 'Blue', option_type: color) }
       let(:option_size) { FactoryGirl.create(:option_value, :name => "s", :presentation => "Small", :option_type => size) }
 
       let(:params) do
-        {:sku=>"002", :name=>"S0388G Bloch Kids Tap Flexww", :description=>"Lace Up Split Sole Leather Tap Shoe",
-          :cost_price=>"29.25", :master_price=>"54.46", :available_on=>"1/1/10", :"tshirt-color"=>"Blue", :"tshirt-size"=>"Small",
-          :on_hand=>"2", :height=>"3", :width=>"4", :depth=>"9", :weight=>"1", :position=>"0", :category=>"Categories >
-          Clothing", :permalink=>"S0388G-bloch-kids-tap-flexewe"
+        { sku: "002", name: "S0388G Bloch Kids Tap Flexww", description: "Lace Up Split Sole Leather Tap Shoe",
+          cost_price: "29.25", price: "54.46", available_on: "1/1/10", :"tshirt-color"=>"Blue", :"tshirt-size"=>"Small",
+          on_hand: "2", height: "3", width: "4", depth: "9", weight: "1", position: "0", category: "Categories > Clothing", permalink: "S0388G-bloch-kids-tap-flexewe"
         }
       end
 
       it "creates a new variant when product already exist" do
         expect do
-          ProductImport.new.send(:create_variant_for, product, :with => params)
+          ProductImport.new.send(:create_variant_for, product, with: params)
         end.to change(product.variants, :count).by(1)
+
         variant = product.variants.last
-        variant.price.to_f.should == 54.46
-        variant.cost_price.to_f.should == 29.25
-        product.option_types.should =~ [size, color]
-        variant.option_values.should =~ [option_size, option_color]
+
+        expect(variant.price.to_f).to eq 54.46
+        expect(variant.cost_price.to_f).to eq 29.25
+        expect(product.option_types =~ [size, color])
+        expect(variant.option_values =~ [option_size, option_color])
       end
 
       it "creates missing option_values for new variant" do
-        ProductImport.new.send(:create_variant_for, product, :with => params.merge(:"tshirt-size" => "Large", :"tshirt-color" => "Yellow"))
+        ProductImport.new.send(:create_variant_for, product, with: params.merge(:"tshirt-size" => "Large", :"tshirt-color" => "Yellow"))
         variant = product.variants.last
-        product.option_types.should =~ [size, color]
-        variant.option_values.should =~ OptionValue.where(:name => %w(Large Yellow))
+        expect(product.option_types =~ [size, color])
+        expect(variant.option_values =~ OptionValue.where(name: %w(Large Yellow)))
       end
 
-      it "should not duplicate option_values for existing variant" do
+      it "duplicates option_values for existing variant" do
         expect do
-          ProductImport.new.send(:create_variant_for, product, :with => params.merge(:"tshirt-size" => "Large", :"tshirt-color" => "Yellow"))
-          ProductImport.new.send(:create_variant_for, product, :with => params.merge(:"tshirt-size" => "Large", :"tshirt-color" => "Yellow"))
+          ProductImport.new.send(:create_variant_for, product, with: params.merge(:"tshirt-size" => "Large", :"tshirt-color" => "Yellow"))
+          ProductImport.new.send(:create_variant_for, product, with: params.merge(:"tshirt-size" => "Large", :"tshirt-color" => "Yellow"))
         end.to change(product.variants, :count).by(1)
         variant = product.variants.last
-        product.option_types.should =~ [size, color]
-        variant.option_values.reload.should =~ OptionValue.where(:name => %w(Large Yellow))
+        expect(product.option_types =~ [size, color])
+        expect(variant.option_values.reload =~ OptionValue.where(name: %w(Large Yellow)))
       end
 
       it "throws an exception when variant with sku exist for another product" do
-        other_product = FactoryGirl.create(:product, :sku => "002")
+        other_product = FactoryGirl.create(:product, sku: "002")
         expect do
-          ProductImport.new.send(:create_variant_for, product, :with => params.merge(:"tshirt-size" => "Large", :"tshirt-color" => "Yellow"))
+          ProductImport.new.send(:create_variant_for, product, with: params.merge(:"tshirt-size" => "Large", :"tshirt-color" => "Yellow"))
         end.to raise_error(SkuError)
       end
     end
 
     describe "#import_data!" do
-      let(:valid_import) { ProductImport.create :data_file => File.new(File.join(File.dirname(__FILE__), '..', 'fixtures', 'valid.csv')) }
-      let(:invalid_import) { ProductImport.create :data_file => File.new(File.join(File.dirname(__FILE__), '..', 'fixtures', 'invalid.csv')) }
+      let(:valid_import) { ProductImport.create data_file: File.new(File.join(File.dirname(__FILE__), '..', 'fixtures', 'valid.csv')) }
+      let(:invalid_import) { ProductImport.create data_file: File.new(File.join(File.dirname(__FILE__), '..', 'fixtures', 'invalid.csv')) }
 
       context "on valid csv" do
         it "create products successfully" do
@@ -79,29 +80,34 @@ module Spree
         end
 
         it "handles product properties" do
-          Property.create :name => "brand", :presentation => "Brand"
-          expect { @import = ProductImport.create(:data_file => File.new(File.join(File.dirname(__FILE__), '..', 'fixtures', 'products_with_properties.csv'))).import_data!(true) }.to change(Product, :count).by(1)
-          (product = Product.last).product_properties.map(&:value).should == ["Rails"]
+          Property.create name: "brand", presentation: "Brand"
+
+          expect { @import = ProductImport.create(data_file: File.new(File.join(File.dirname(__FILE__), '..', 'fixtures', 'products_with_properties.csv'))).import_data!(true) }.to change(Product, :count).by(3)
+
+          product = Product.last
+          expect(product.product_properties.map(&:value)).to eq(['Rails'])
+          # Commented out because this is a properties test, not a variant one.
+          # expect(product.variants.count).to eq 2
         end
 
         it "sets state to completed" do
           valid_import.import_data!
-          valid_import.reload.state.should == "completed"
+          expect(valid_import.reload.state).to eq "completed"
         end
       end
 
       context "on invalid csv" do
-        it "should not tracks product created ids" do
+        it "doesn't track product's created ids" do
           expect { invalid_import.import_data! }.to raise_error(ImportError)
           invalid_import.reload
-          invalid_import.product_ids.should be_empty
-          invalid_import.products.should be_empty
+          expect(invalid_import.product_ids).to be_empty
+          expect(invalid_import.products).to be_empty
         end
 
         context "when params = true (transaction)" do
           it "rollback transation" do
             expect { invalid_import.import_data! }.to raise_error(ImportError)
-            Product.count.should == 0
+            expect(Product.count).to eq 0
           end
 
           it "sets state to failed" do
@@ -110,25 +116,31 @@ module Spree
           end
         end
 
-        context "when params = false (no transaction)" do
+        context "when params = false (no transaction)", focus: true do
           it "sql are permanent" do
             expect { invalid_import.import_data!(false) }.to raise_error(ImportError)
-            Product.count.should == 1
+            # How many invalid products are there in the fixture?
+            # 
+            expect(Product.count).to eq 1
           end
 
           it "sets state to failed" do
             expect { invalid_import.import_data!(false) }.to raise_error(ImportError)
-            invalid_import.reload.state.should == "failed"
+            expect(invalid_import.reload.state).to eq "failed"
           end
         end
       end
     end
 
     describe "#destroy_products" do
-      it "should also destroy associations" do
-        expect { (@import = ProductImport.create(:data_file => File.new(File.join(File.dirname(__FILE__), '..', 'fixtures', 'products_with_properties.csv')))).import_data!(true) }.to change(Product, :count).by(1)
+      it "destroys associations" do
+        expect do
+          (@import = ProductImport.create(
+            data_file: File.new(File.join(File.dirname(__FILE__), '..', 'fixtures', 'products_with_properties.csv'))))
+            .import_data!(true)
+        end.to change(Product, :count).by(3)
         @import.destroy
-        Variant.count.should == 0
+        expect(Variant.count).to eq 0
       end
     end
   end
