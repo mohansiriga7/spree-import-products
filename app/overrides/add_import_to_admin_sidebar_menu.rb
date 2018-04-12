e90ee5a6666fa99d133016ea6d54657ab50e2db1
mohansiriga7 <ms7.developer@gmail.com>
2018-04-12 16:44:53 +0530

    Using import_sidebar_menu shared partial in deface

--- app/overrides/add_import_to_admin_sidebar_menu.rb
+++ app/overrides/add_import_to_admin_sidebar_menu.rb
@@ -9,6 +9,7 @@ Deface::Override.new(
     virtual_path: "spree/admin/shared/sub_menu/_product",
     name: "product_import_admin_sidebar_menu",
     insert_bottom: "[data-hook='admin_product_sub_tabs']",
-		text: "<%= configurations_sidebar_menu_item t(:product_imports), spree.admin_product_imports_url %>"
-    #partial: 'spree/admin/shared/import_sidebar_menu'
+		#text: "<%= configurations_sidebar_menu_item t(:product_imports), spree.admin_product_imports_url %>"
+    partial: 'spree/admin/shared/import_sidebar_menu'
 )
+
